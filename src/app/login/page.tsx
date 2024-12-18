fc6fae8a5a8f8e6a24049629e4fbfe25965bf939
Gonzalo Miranda <gjmmss@gmail.com>
2024-12-18 16:18:47 +0100

    login style modifications

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,16 @@
 import LoginForm from "@/components/login";
+import frFR from "@/lang/fr-FR";
 
 export default function LoginPage() {
+  const t = frFR;
   return (
     // <main className="flex h-[90vh] w-[90vw] items-center justify-center">
     <main className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black">
-      <div className="p-5"></div>
-      <div className="w-[30vw] space-y-5 rounded-md border bg-muted/50 p-3">
-        <span className="text-2xl">Login</span>
+      {/* <div className="p-5"></div> */}
+      <div className="flex h-[20rem] w-[30vw] flex-col justify-center space-y-7 rounded-md border bg-muted/50 p-3">
+        <div className="flex w-full justify-center">
+          <span className="text-2xl">{t.shared.login}</span>
+        </div>
         <LoginForm />
       </div>
     </main>
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,7 +3,10 @@
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Icon from "@/components/common/icon";
+import isValidIconName from "@/functions/isValidIconName";
 import { authenticate } from "@/lib/actions";
+import frFR from "@/lang/fr-FR";
 
 type LoginFormData = {
   userName: string;
@@ -11,6 +14,7 @@ type LoginFormData = {
 };
 
 export default function LoginForm() {
+  const t = frFR;
   const [errorLogInMessage, setErrorLogInMessage] = useState<
     string | undefined
   >(undefined);
@@ -52,28 +56,53 @@ export default function LoginForm() {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col space-y-5">
-      <div>
-        <span>User :</span>
+      <div className="flex items-center">
+        <Icon
+          name={
+            isValidIconName("MdPerson") ? "MdPerson" : "MdOutlineNotInterested"
+          }
+          className="text-xl"
+        />
         <Input
           id="userName"
           name="userName"
-          placeholder="User name"
+          placeholder={`${t.login.user}`}
+          variant="lined"
           className="w-full"
           onChange={handleChange}
           value={formData.userName}
         />
       </div>
-      <div>
-        <span>Password :</span>
-        <Input
-          id="password"
-          name="password"
-          placeholder="Password"
-          type="password"
-          className="w-full"
-          onChange={handleChange}
-          value={formData.password}
-        />
+      <div className="space-y-1">
+        <div className="flex items-center">
+          <Icon
+            name={
+              isValidIconName("MdPassword")
+                ? "MdPassword"
+                : "MdOutlineNotInterested"
+            }
+            className="text-xl"
+          />
+          <Input
+            id="password"
+            name="password"
+            placeholder={`${t.login.password}`}
+            type="password"
+            variant="lined"
+            className="w-full"
+            onChange={handleChange}
+            value={formData.password}
+          />
+        </div>
+        <div
+          className="flex h-5 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorLogInMessage && (
+            <p className="text-sm text-red-500">{errorLogInMessage}</p>
+          )}
+        </div>
       </div>
       <div className="flex justify-center">
         <Button
@@ -82,17 +111,8 @@ export default function LoginForm() {
           className="w-[40%]"
           disabled={isPending}
         >
-          Login
+          {t.shared.login}
         </Button>
-      </div>
-      <div
-        className="flex h-8 items-end space-x-1"
-        aria-live="polite"
-        aria-atomic="true"
-      >
-        {errorLogInMessage && (
-          <p className="text-sm text-red-500">{errorLogInMessage}</p>
-        )}
       </div>
     </form>
   );
