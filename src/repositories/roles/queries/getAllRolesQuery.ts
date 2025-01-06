5edcc364efbf2ffa79eacbba6401b07a6f47cb9d
Gonzalo Miranda <gjmmss@gmail.com>
2025-01-06 18:16:30 +0100

    cleaning roles repositories commented code

--- src/repositories/roles/queries/getAllRolesQuery.ts
+++ src/repositories/roles/queries/getAllRolesQuery.ts
@@ -1,19 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-// import { Query } from "@/interfaces/query";
 import { RolesViewModel } from "../rolesViewModel";
 
 const prisma = new PrismaClient();
-
-// export class getAllRolesQuery implements Query<RolesViewModel[]> {
-//   async execute(): Promise<RolesViewModel[]> {
-//     const result = await prisma.roles.findMany({});
-
-//     return result.map((roles: RolesViewModel) => ({
-//       RoleId: roles.RoleId,
-//       Name: roles.Name,
-//     }));
-//   }
-// }
 
 const getAllRolesQuery = async () => {
   const query = await prisma.roles.findMany({});
--- src/repositories/roles/queries/getRoleByRoleIdQuery.ts
+++ src/repositories/roles/queries/getRoleByRoleIdQuery.ts
@@ -1,17 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-// import { Query } from "@/interfaces/query";
-// import { RoleViewModel } from "@/repositories/roles/rolesViewModel";
 
 const prisma = new PrismaClient();
-
-// export class getRoleByRoleIdQuery implements Query<RoleViewModel, number> {
-//   async execute(roleId: number): Promise<RoleViewModel> {
-//     const result = await prisma.roles.findFirstOrThrow({
-//       where: { RoleId: roleId },
-//     });
-//     return result;
-//   }
-// }
 
 const getRoleByRoleIdQuery = async (roleId: number) => {
   return await prisma.roles.findFirstOrThrow({
