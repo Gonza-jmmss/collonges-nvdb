685b93ede0ebcbc205661a1413739a890294e739
Gonzalo Miranda <gjmmss@gmail.com>
2025-01-06 18:10:19 +0100

    changing users repository functions calls getUserByUserNameQuery

--- src/repositories/users/queries/getUserByUserNameQuery.ts
+++ src/repositories/users/queries/getUserByUserNameQuery.ts
@@ -1,23 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { Query } from "@/interfaces/query";
-import { UserViewModel } from "@/repositories/users/usersViewModel";
 
 const prisma = new PrismaClient();
 
-export class getUserByUserNameQuery implements Query<UserViewModel, string> {
-  async execute(userName: string): Promise<UserViewModel> {
-    const result = await prisma.users.findFirstOrThrow({
-      where: { UserName: userName },
-      include: {
-        Roles: true,
-      },
-    });
-    return {
-      UserId: result.UserId,
-      UserName: result.UserName,
-      Password: "",
-      RoleId: result.Roles.RoleId,
-      RoleName: result.Roles.Name,
-    };
-  }
-}
+const getUserByUserNameQuery = async (userName: string) => {
+  return await prisma.users.findFirstOrThrow({
+    where: { UserName: userName },
+    include: {
+      Roles: true,
+    },
+  });
+};
+
+export default getUserByUserNameQuery;
