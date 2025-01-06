675ebbc19d47fb118f787c7a910b0747cfc4f86e
Gonzalo Miranda <gjmmss@gmail.com>
2025-01-06 18:16:50 +0100

    cleaning users repositories commented code

--- src/repositories/users/usersViewModel.ts
+++ src/repositories/users/usersViewModel.ts
@@ -22,39 +22,3 @@ export type UserViewModel = {
   RoleId: number;
   RoleName: string;
 } | null;
-
-///////
-
-// export type UserViewModel = {
-//   UserId: number;
-//   UserName: string;
-//   Password: string;
-//   RoleId: number;
-//   RoleName: string;
-//   UpdatedAt?: Date | null;
-//   CreatedAt?: Date | null;
-// };
-
-export type UserCreateViewModel = {
-  UserId: number;
-  UserName: string;
-  Password: string;
-  RoleId: number;
-  UpdatedAt?: Date | null;
-  CreatedAt?: Date | null;
-};
-
-export interface UserCreateInterface {
-  UserName: string;
-  Password: string;
-  RoleId: number;
-}
-
-export interface UserUpdateViewModel {
-  UserId: number;
-  UserName: string;
-}
-
-export interface UserDeleteViewModel {
-  UserId: number;
-}
