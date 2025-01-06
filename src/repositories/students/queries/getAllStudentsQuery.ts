263c6713cac5822843bb5d749df998957e33b1e3
Gonzalo Miranda <gjmmss@gmail.com>
2025-01-06 18:16:39 +0100

    cleaning students repositories commented code

--- src/repositories/students/queries/getAllStudentsQuery.ts
+++ src/repositories/students/queries/getAllStudentsQuery.ts
@@ -1,47 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-// import { Query } from "@/interfaces/query";
-import { StudentsViewModel, StudentsMapViewModel } from "../studentsViewModel";
+import { StudentsMapViewModel } from "../studentsViewModel";
 
 const prisma = new PrismaClient();
-
-// export class getAllStudentsQuery implements Query<StudentsViewModel[]> {
-//   async execute(): Promise<StudentsViewModel[]> {
-//     const result = await prisma.students.findMany({
-//       orderBy: [
-//         {
-//           IsACA: "desc",
-//         },
-//         {
-//           Persons: {
-//             LastName: "asc",
-//           },
-//         },
-//       ],
-//       include: {
-//         Persons: {
-//           select: {
-//             AlternativeName: true,
-//             DBaseCode: true,
-//           },
-//         },
-//         StudentTypes: {
-//           select: {
-//             Name: true,
-//           },
-//         },
-//       },
-//     });
-
-//     return result.map((student: StudentsMapViewModel) => ({
-//       ...student,
-//       StudentName: student.Persons?.AlternativeName,
-//       StudentType: student.StudentTypes?.Name,
-//       DBaseCode: student.Persons?.DBaseCode,
-//       // Persons: undefined,
-//       // StudentTypes: undefined,
-//     }));
-//   }
-// }
 
 const getAllStudentsQuery = async () => {
   const query = await prisma.students.findMany({
