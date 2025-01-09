d53d716bf352f743830b3c3179b6d8a347ba547c
Gonzalo Miranda <gjmmss@gmail.com>
2025-01-09 17:52:34 +0100

    Breadcrumbs routing this useRouter instead of href

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -11,10 +11,12 @@ import {
 } from "@/components/ui/breadcrumb";
 import ThemeToggle from "./common/themeToggle";
 import { Fragment } from "react";
+import { useRouter } from "next/navigation";
 import frFR from "@/lang/fr-FR";
 
 export default function Breadcrumbs() {
   const t = frFR;
+  const router = useRouter();
   const segments = useSelectedLayoutSegments();
   const breadcrumbElements = ["home"].concat(segments);
 
@@ -26,13 +28,19 @@ export default function Breadcrumbs() {
             <Fragment key={index}>
               <BreadcrumbItem>
                 {index === breadcrumbElements.length - 1 ? (
-                  <BreadcrumbPage className={`text-primary`}>
+                  <BreadcrumbPage className={`cursor-default text-primary`}>
                     {`${isNaN(Number(segment)) ? t.breadcrumbs[segment as keyof typeof t.breadcrumbs] : segment}`}
                   </BreadcrumbPage>
                 ) : (
                   <>
                     <BreadcrumbLink
-                      href={`/${breadcrumbElements.slice(1, index + 1).join("/")}`}
+                      onClick={() =>
+                        router.push(
+                          `/${breadcrumbElements.slice(1, index + 1).join("/")}`,
+                        )
+                      }
+                      className="cursor-pointer"
+                      // href={`/${breadcrumbElements.slice(1, index + 1).join("/")}`}
                     >
                       {`${isNaN(Number(segment)) ? t.breadcrumbs[segment as keyof typeof t.breadcrumbs] : segment}`}
                     </BreadcrumbLink>
