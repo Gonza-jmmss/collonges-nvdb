74bbd26b777ff5c5ff7fc89a6b802e1b6e3d4ab8
Gonzalo Miranda <gjmmss@gmail.com>
2025-02-07 11:01:12 +0100

    login page style modification

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,9 +5,9 @@ export default function LoginPage() {
   const t = frFR;
   return (
     // <main className="flex h-[90vh] w-[90vw] items-center justify-center">
-    <main className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black">
+    <main className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-background">
       {/* <div className="p-5"></div> */}
-      <div className="flex h-[20rem] w-[30vw] flex-col justify-center space-y-7 rounded-md border bg-muted/50 p-3">
+      <div className="flex h-[20rem] w-[30vw] flex-col justify-center space-y-7 rounded-md border bg-muted/50 p-3 shadow">
         <div className="flex w-full justify-center">
           <span className="text-2xl">{t.shared.login}</span>
         </div>
