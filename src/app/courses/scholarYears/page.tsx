71072817a0a169460994c660cd16b19808c508a3
Gonzalo Miranda <gjmmss@gmail.com>
2025-04-18 11:23:50 +0200

    ScholarYearsPage adding back button

--- src/app/courses/scholarYears/page.tsx
+++ src/app/courses/scholarYears/page.tsx
@@ -1,5 +1,8 @@
 import ScholarYearsTable from "./scholarYearsTable";
 import getAllScholarYearsTableQuery from "@/repositories/scholarYears/queries/getAllScholarYearsQueryTable";
+import Icon from "@/components/common/icon";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import frFR from "@/lang/fr-FR";
 
 export default async function ScholarYearsPage({}: {}) {
@@ -8,14 +11,17 @@ export default async function ScholarYearsPage({}: {}) {
   const scholarYears = await getAllScholarYearsTableQuery();
 
   return (
-    <main>
-      <div className="mt-3 w-[80vw]">
-        <div className="flex justify-between space-x-3">
-          <span className="text-xl font-semibold">{t.scholarYears.title}</span>
-        </div>
-        <ScholarYearsTable scholarYears={scholarYears} />
-        {/* <pre>{JSON.stringify(scholarYears, null, 2)}</pre> */}
+    <main className="relative mt-3 w-[80vw]">
+      <Button asChild className={`absolute -left-16 -top-1`} variant="ghost">
+        <Link href={`/courses`}>
+          <Icon name={"MdArrowBack"} className="text-xl" />
+        </Link>
+      </Button>
+      <div className="flex justify-between space-x-3">
+        <span className="text-xl font-semibold">{t.scholarYears.title}</span>
       </div>
+      <ScholarYearsTable scholarYears={scholarYears} />
+      {/* <pre>{JSON.stringify(scholarYears, null, 2)}</pre> */}
     </main>
   );
 }
