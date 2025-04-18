cd04e0051076c6df04bdad400b8bfcaa80242044
Gonzalo Miranda <gjmmss@gmail.com>
2025-04-18 09:21:50 +0200

    courses condition to role functionality restriction

--- src/app/courses/courses/page.tsx
+++ src/app/courses/courses/page.tsx
@@ -1,5 +1,9 @@
 import CoursesTable from "./coursesTable";
 import getAllCoursesQuery from "@/repositories/courses/queries/getAllCoursesQuery";
+import Icon from "@/components/common/icon";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { auth } from "@/utils/auth";
 import frFR from "@/lang/fr-FR";
 
 export default async function CoursesPage({
@@ -8,11 +12,13 @@ export default async function CoursesPage({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const t = frFR;
+  const session = await auth();
+
+  const userRoleName = session?.user.userData.Roles.Name;
 
   const pageIndex = searchParams?.pageIndex
     ? parseInt(searchParams.pageIndex as string)
     : 0;
-
   const pageSize = searchParams?.pageSize
     ? parseInt(searchParams.pageSize as string)
     : 10;
@@ -36,21 +42,25 @@ export default async function CoursesPage({
   });
 
   return (
-    <main>
-      <div className="mt-3 w-[80vw]">
-        <div className="flex justify-between space-x-3">
-          <span className="text-xl font-semibold">{t.courses.title}</span>
-        </div>
-        <CoursesTable
-          coursesData={courses}
-          isEnabledSelected={isEnabledParam}
-          periodNumberSelected={periodNumberParam}
-          pageIndex={pageIndex}
-          pageSize={pageSize}
-          urlParams={searchParams}
-        />
-        {/* <pre>{JSON.stringify(students, null, 2)}</pre> */}
+    <main className="relative mt-3 w-[80vw]">
+      <Button asChild className={`absolute -left-16 -top-1`} variant="ghost">
+        <Link href={`/courses`}>
+          <Icon name={"MdArrowBack"} className="text-xl" />
+        </Link>
+      </Button>
+      <div className="flex justify-between space-x-3">
+        <span className="text-xl font-semibold">{t.courses.title}</span>
       </div>
+      <CoursesTable
+        coursesData={courses}
+        isEnabledSelected={isEnabledParam}
+        periodNumberSelected={periodNumberParam}
+        userRoleName={userRoleName}
+        pageIndex={pageIndex}
+        pageSize={pageSize}
+        urlParams={searchParams}
+      />
+      {/* <pre>{JSON.stringify(students, null, 2)}</pre> */}
     </main>
   );
 }
