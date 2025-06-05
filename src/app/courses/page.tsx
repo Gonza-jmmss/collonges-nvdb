0730ce29f7a65b24545d264e0185e6b8c09aa7b3
Gonzalo Miranda <gjmmss@gmail.com>
2025-06-05 16:49:16 +0200

    courses page removing comented code

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -30,7 +30,6 @@ export default async function Settings() {
           <span className="text-4xl font-bold">{t.breadcrumbs.courses}</span>
         </div>
         {/* <pre>{JSON.stringify(moduleElements, null, 2)}</pre> */}
-        {/* <div className={`mt-10 grid grid-cols-5 justify-items-center gap-8`}> */}
         <div
           className={`-ml-8 mt-10 flex flex-wrap justify-center space-x-8 space-y-8`}
         >
